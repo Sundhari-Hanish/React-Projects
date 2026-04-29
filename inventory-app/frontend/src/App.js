dec58bf447f8db93b39b4ad977020e4c8d0a6323
Sundhari Hanish <sundharihanish@gmail.com>
2026-04-29 09:00:58 +0530

    Create App.js

---
+++ inventory-app/frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import AddProduct from "./components/AddProduct";
+import ProductList from "./components/ProductList";
+
+function App() {
+  return (
+    <div>
+      <h1>Inventory Management</h1>
+      <AddProduct />
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
